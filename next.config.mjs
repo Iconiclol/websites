1ce2d7499b36ed3450e9875bc0bdb46cb4d1703d
Iconiclol <101067401+Iconiclol@users.noreply.github.com>
2025-06-28 19:34:12 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/websites' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/websites/' : '',
 }
 
 export default nextConfig
